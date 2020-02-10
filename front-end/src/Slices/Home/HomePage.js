abee88738fd57b628d73f1734de18b8884bb1385
fraserb99 <44883743+fraserb99@users.noreply.github.com>
2020-02-10 12:26:45 +0000

    ci test

--- front-end/src/Slices/Home/HomePage.js
+++ front-end/src/Slices/Home/HomePage.js
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 export const HomePage = () => (
     <div>
         <h1>Home</h1>
+        <p>
+            This is the homepage
+        </p>
         <Link to='/compostdata'>Compost Data</Link>
         <Link to='devices'>Devices</Link>
     </div>
